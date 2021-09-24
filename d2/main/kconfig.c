bdaa060d67fa39c5691f047d9ffefa5ca7b7fbcc
Arne de Bruijn <mail@arnedebruijn.nl>
2021-09-24 21:06:15 +0200

    Fix Controls.mouse_axis out of bounds

--- d2/main/kconfig.c
+++ d2/main/kconfig.c
@@ -1535,10 +1535,11 @@ void kconfig_read_controls(d_event *event, int automap_flag)
 	else
 		Controls.vertical_thrust_time -= (Controls.joy_axis[kc_joystick[19].value]*PlayerCfg.JoystickSens[3]*undercalibrate_scale(PlayerCfg.JoystickUndercalibrate[3]))/8;
 	// From mouse...
-	if ( !kc_mouse[20].value )		// If not inverted...
-		Controls.vertical_thrust_time += (Controls.mouse_axis[kc_mouse[19].value]*PlayerCfg.MouseSens[3])/8;
-	else
-		Controls.vertical_thrust_time -= (Controls.mouse_axis[kc_mouse[19].value]*PlayerCfg.MouseSens[3])/8;
+	if ( kc_mouse[19].value != 255 )
+		if ( !kc_mouse[20].value )		// If not inverted...
+			Controls.vertical_thrust_time += (Controls.mouse_axis[kc_mouse[19].value]*PlayerCfg.MouseSens[3])/8;
+		else
+			Controls.vertical_thrust_time -= (Controls.mouse_axis[kc_mouse[19].value]*PlayerCfg.MouseSens[3])/8;
 
 	//---------- Read heading_time -----------
 	if (!Controls.slide_on_state && !Controls.bank_on_state)
@@ -1630,10 +1631,11 @@ void kconfig_read_controls(d_event *event, int automap_flag)
 	else
 		Controls.sideways_thrust_time -= (Controls.joy_axis[kc_joystick[17].value]*PlayerCfg.JoystickSens[2]*undercalibrate_scale(PlayerCfg.JoystickUndercalibrate[2]))/8;
 	// From mouse...
-	if ( !kc_mouse[18].value )		// If not inverted...
-		Controls.sideways_thrust_time += (Controls.mouse_axis[kc_mouse[17].value]*PlayerCfg.MouseSens[2])/8;
-	else
-		Controls.sideways_thrust_time -= (Controls.mouse_axis[kc_mouse[17].value]*PlayerCfg.MouseSens[2])/8;
+	if ( kc_mouse[17].value != 255 )
+		if ( !kc_mouse[18].value )		// If not inverted...
+			Controls.sideways_thrust_time += (Controls.mouse_axis[kc_mouse[17].value]*PlayerCfg.MouseSens[2])/8;
+		else
+			Controls.sideways_thrust_time -= (Controls.mouse_axis[kc_mouse[17].value]*PlayerCfg.MouseSens[2])/8;
 
 	//----------- Read bank_time -----------------
 	if ( Controls.bank_on_state )
@@ -1692,10 +1694,11 @@ void kconfig_read_controls(d_event *event, int automap_flag)
 	else
 		Controls.bank_time += (Controls.joy_axis[kc_joystick[21].value]*PlayerCfg.JoystickSens[4]*undercalibrate_scale(PlayerCfg.JoystickUndercalibrate[4]))/8;
 	// From mouse...
-	if ( !kc_mouse[22].value )		// If not inverted...
-		Controls.bank_time += (Controls.mouse_axis[kc_mouse[21].value]*PlayerCfg.MouseSens[4])/8;
-	else
-		Controls.bank_time -= (Controls.mouse_axis[kc_mouse[21].value]*PlayerCfg.MouseSens[4])/8;
+	if ( kc_mouse[21].value != 255 )
+		if ( !kc_mouse[22].value )		// If not inverted...
+			Controls.bank_time += (Controls.mouse_axis[kc_mouse[21].value]*PlayerCfg.MouseSens[4])/8;
+		else
+			Controls.bank_time -= (Controls.mouse_axis[kc_mouse[21].value]*PlayerCfg.MouseSens[4])/8;
 
 	//----------- Read forward_thrust_time -------------
 	// From keyboard/buttons...
@@ -1707,10 +1710,11 @@ void kconfig_read_controls(d_event *event, int automap_flag)
 	else
 		Controls.forward_thrust_time += (Controls.joy_axis[kc_joystick[23].value]*PlayerCfg.JoystickSens[5]*undercalibrate_scale(PlayerCfg.JoystickUndercalibrate[5]))/8;
 	// From mouse...
-	if ( !kc_mouse[24].value )		// If not inverted...
-		Controls.forward_thrust_time -= (Controls.mouse_axis[kc_mouse[23].value]*PlayerCfg.MouseSens[5])/8;
-	else
-		Controls.forward_thrust_time += (Controls.mouse_axis[kc_mouse[23].value]*PlayerCfg.MouseSens[5])/8;
+	if ( kc_mouse[23].value != 255 )
+		if ( !kc_mouse[24].value )		// If not inverted...
+			Controls.forward_thrust_time -= (Controls.mouse_axis[kc_mouse[23].value]*PlayerCfg.MouseSens[5])/8;
+		else
+			Controls.forward_thrust_time += (Controls.mouse_axis[kc_mouse[23].value]*PlayerCfg.MouseSens[5])/8;
 
 	//----------- Read cruise-control-type of throttle.
 	// For LoNi -- have cruise go to 100% instantly.  Will add option if anyone cares about making it slow.
